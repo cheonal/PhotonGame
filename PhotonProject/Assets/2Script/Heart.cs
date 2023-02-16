3d1f515baba93409f3ea2244a5c29e01e24acfdb
cheonal <bulewing99@naver.com>
2023-02-16 15:58:27 +0900

    230216 자꾸 날라가서 백업

---
+++ PhotonProject/Assets/2Script/Heart.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class Heart : MonoBehaviourPunCallbacks
+{
+    public PhotonView PV;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && PV.IsMine)
+        {
+            PV.RPC("DestroyRPC", RpcTarget.AllBuffered);
+        }
+        if (!PV.IsMine && collision.tag == "Player" && collision.GetComponent<PhotonView>().IsMine) // 느린쪽에 맞춰서 HIT판정
+        {
+            NetworkManager.networkManager.GetHeart();
+            collision.GetComponent<Player>().Heal();
+            PV.RPC("DestroyRPC", RpcTarget.AllBuffered);
+        }
+    }
+    [PunRPC]
+    void DestroyRPC()
+    {
+        Destroy(gameObject);
+    }
+}
